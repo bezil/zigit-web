48dc4b49dedec0d381dea8b6951c395d97ea76a7
Basil <basil.george@aspireapp.com>
2024-01-06 19:04:56 +0530

    update images

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,8 +1,9 @@
 # Clear all files from docs
 rm -rf docs/*
 
-# Create a file named CNAME
+# Create a file named CNAME and folders
 cd docs
+mkdir img
 echo "zigit.io" > CNAME
 cd ..
 
@@ -11,4 +12,4 @@ trunk build --release
 
 # Copy files from dist to docs
 cp -r dist/* docs/
-cp -r dist/img/* docs/img/
+cp -r img/* docs/img/
